5a68bd048ab68327bb1a7fa1a374d6e6c8456923
Varlorg <varlorg@gmail.com>
2017-12-07 22:20:25 +0100

    uncrustify SQLiteBase

--- app/src/main/java/app/varlorg/unote/SQLiteBase.java
+++ app/src/main/java/app/varlorg/unote/SQLiteBase.java
@@ -8,18 +8,17 @@ import android.util.Log;
 
 public class SQLiteBase extends SQLiteOpenHelper
 {
-    private static final String TABLE_NOTES = "table_notes";
-    private static final String COL_ID = "ID";
-    private static final String COL_NOTE = "Note";
-    private static final String COL_TITRE = "Titre";
-    private static final String COL_DATECREATION = "Date_creation";
+    private static final String TABLE_NOTES          = "table_notes";
+    private static final String COL_ID               = "ID";
+    private static final String COL_NOTE             = "Note";
+    private static final String COL_TITRE            = "Titre";
+    private static final String COL_DATECREATION     = "Date_creation";
     private static final String COL_DATEMODIFICATION = "Date_modification";
-    private static final String COL_PASSWORD = "password";
-
-    private static final String TEXT_NOT_NULL = " TEXT NOT NULL, ";
-    private static final String CREATE_BDD = "CREATE TABLE " + TABLE_NOTES + " ("
-            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NOTE + TEXT_NOT_NULL
-            + COL_TITRE + TEXT_NOT_NULL + COL_DATECREATION + TEXT_NOT_NULL + COL_DATEMODIFICATION +  TEXT_NOT_NULL  + COL_PASSWORD  + " VARCHAR(41) );";
+    private static final String COL_PASSWORD         = "password";
+    private static final String TEXT_NOT_NULL        = " TEXT NOT NULL, ";
+    private static final String CREATE_BDD           = "CREATE TABLE " + TABLE_NOTES + " ("
+                                                       + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NOTE + TEXT_NOT_NULL
+                                                       + COL_TITRE + TEXT_NOT_NULL + COL_DATECREATION + TEXT_NOT_NULL + COL_DATEMODIFICATION + TEXT_NOT_NULL + COL_PASSWORD + " VARCHAR(41) );";
 
     public SQLiteBase(Context context, String name, CursorFactory factory, int version)
     {
@@ -36,7 +35,9 @@ public class SQLiteBase extends SQLiteOpenHelper
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
         //Create password column when upgrading old version ( < 1.1 )
-        if (oldVersion < 2 )
-            db.execSQL("ALTER TABLE " +  TABLE_NOTES + " ADD COLUMN " + COL_PASSWORD +" VARCHAR(41);");
+        if (oldVersion < 2)
+        {
+            db.execSQL("ALTER TABLE " + TABLE_NOTES + " ADD COLUMN " + COL_PASSWORD + " VARCHAR(41);");
+        }
     }
 }
