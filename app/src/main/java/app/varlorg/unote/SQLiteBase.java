a492f18c777b7bb1076f03d97e36727798b70b12
Varlorg <varlorg@gmail.com>
2017-12-09 14:31:19 +0100

    Remove log import from SQLiteBase class

--- app/src/main/java/app/varlorg/unote/SQLiteBase.java
+++ app/src/main/java/app/varlorg/unote/SQLiteBase.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
-import android.util.Log;
 
 public class SQLiteBase extends SQLiteOpenHelper
 {
