17cfbb81d833538ce61eeef252ed0cbb3e3b39a9
Varlorg <varlorg@gmail.com>
2025-02-01 22:51:45 +0100

    preference: split into multiples screens

---
+++ app/src/main/java/app/varlorg/unote/PreferenceEdition.java
@@ -0,0 +1,43 @@
+package app.varlorg.unote;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class PreferenceEdition extends PreferenceActivity {
+    Bundle savedInstanceState;
+
+    private int textSize;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        textSize = Integer.parseInt(pref.getString("pref_sizeNote", "18"));
+        if ( textSize == -1 )
+        {
+            textSize = Integer.parseInt(pref.getString("pref_sizeNote_custom", "18"));
+        }
+
+        NoteMain.setUi(this, pref, getApplicationContext(), getWindow());
+
+        this.savedInstanceState = savedInstanceState;
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preference_edition);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        Intent intent = new Intent(this, Preference.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish();
+    }
+}
